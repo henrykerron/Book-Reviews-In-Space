4f58e74eeb57a451a151e2bdece2b1f82ca70efa
henrykerron <henrykerron@gmail.com>
2014-12-27 12:20:59 -0500

    Added validations and error messages to all fields for posting a book feature

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ActiveRecord::Base
+	validates :title, presence: true
+	validates :title, length: {minimum: 1}
+	validates :description, presence: true
+	validates :description, length: {minimum: 10}
+	validates :url, presence: true
 end
--- spec/features/user_posts_book_as_link_spec.rb
+++ spec/features/user_posts_book_as_link_spec.rb
@@ -8,20 +8,31 @@ feature "post a book as a link", %q(
 
 	Acceptance Criteria
 
-	[] I must provide a title that is at least 1 character long
-	[] I must provide a description that is at least 10 characters long
-	[] I must provide a valid URL to the book
-	[] I must be presented with errors if I fill out the form incorrectly
+	[x] I must provide a title that is at least 1 character long
+	[x] I must provide a description that is at least 10 characters long
+	[x] I must provide a valid URL to the book
+	[x] I must be presented with errors if I fill out the form incorrectly
 	) do
 
-	
-		scenario 'user posts a book' do
+		before(:each) do
 			visit new_book_path
+		end
+
+		scenario 'user posts a book' do
 			fill_in "Title", with: "Hunger Games"
 			fill_in "Description", with: "1st Book in the Series"
-			fill_in "URL", with: "https://www.goodreads.com/book/show/2767052-the-hunger-games?from_search=true"
+			fill_in "Url", with: "https://www.goodreads.com/book/show/2767052-the-hunger-games?from_search=true"
 			click_on "Add Book"
 
 			expect(page).to have_content("New book submitted successfully!")
+		end
+
+		scenario 'user gets errors if form is filled out incorrectly' do
+			visit new_book_path
+			click_on "Add Book"
+
+			expect(page).to have_content("Title is too short (minimum is 1 characters)")
+			expect(page).to have_content("Description is too short (minimum is 10 characters)")
+			expect(page).to have_content("Url can't be blank")
 		end
 end
